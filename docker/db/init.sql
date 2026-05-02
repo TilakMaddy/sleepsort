55785f4398b770aebb5bd2caf30418d570f86dad
TilakMaddy <tilak.madichetti@gmail.com>
2026-05-02 14:18:28 +0530

    chore: config in compose db

--- docker/db/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS sorts (
-    id              SERIAL PRIMARY KEY,
-    input_numbers   INT[] NOT NULL,
-    sorted_numbers  INT[] NOT NULL,
-    created_at      TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
